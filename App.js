8f4b275bc0856f9472e04defae328ea500b8ef4b
Henni Pätynen <henni.patynen@myy.haaga-helia.fi>
2023-01-22 20:42:02 +0200

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Alert, Image } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const[text, setText] = useState('');
+
+  const pressed =()=>{
+  Alert.alert('You wrote ' + text)
+};
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+      style={{ width:200, height:100}}
+      source={{url: 'https://th.bing.com/th/id/R.c0605d85e63969417cceebb829839025?rik=MZh5NmMVf%2bOrEg&pid=ImgRaw&r=0'}}
+      />
+      <TextInput
+      style={styles.teksti}
+      onChangeText={text=> setText(text)}
+      value={text}
+      />
+      <Button title ="Press me" onPress={pressed}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +35,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  teksti:{
+    width:200, 
+    borderColor:'grey', 
+    borderWidth:2
+  }
 });
